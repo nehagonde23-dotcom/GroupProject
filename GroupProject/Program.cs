80e3c0ae458cbf75541ab7121dfe11ea975c1514
nehagonde23-dotcom <nehagonde23@gmail.com>
2026-05-11 12:49:34 +0530

    Add project files.

---
+++ GroupProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject
+{
+    internal class Program
+    {
+        public void Add()
+        {
+            int a = 10;
+            int b = a + a;
+            Console.WriteLine("Add=" + b);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
